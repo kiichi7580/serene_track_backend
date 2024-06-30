b528edad11f22ddb54c5375f5c6cd5d8ba8daef2
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-30 10:59:18 +0900

    Usersクラスを作成、TodosクラスにUsersクラスと繋ぐforeignkeyを追加

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,6 +1,20 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from sqlalchemy.sql import func
+
+
+class Users(Base):
+  __tablename__ = 'users'
+
+  id = Column(Integer, primary_key=True, index=True)
+  name = Column(String, unique=True)
+  email = Column(String, unique=True)
+  createAt = Column(DateTime, default=func.now())
+  photoUrl = Column(String)
+  hashed_password = Column(String)
+  is_active = Column(Boolean, default=True)
+  role = Column(String)
+
 
 class Todos(Base):
   __tablename__ = 'todos'
@@ -11,4 +25,4 @@ class Todos(Base):
   complete = Column(Boolean, default=False)
   category_id = Column(String)
   date = Column(DateTime, default=func.now())
-  user_id = Column(String)
+  owner_id = Column(Integer, ForeignKey("users.id"))
