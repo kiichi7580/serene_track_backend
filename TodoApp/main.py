9eadf80ffec396cfbb9b0fd221c103b5d5bb97bc
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-30 11:03:35 +0900

    APIRouterを使用して管理

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,23 +1,11 @@
-from typing import Annotated
-from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 import models
-from models import Todos
-from database import engine, SessionLocal
+from database import engine
+from routers import auth, todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-def get_db():
-  db = SessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
-
-db_dependency = Annotated[Session, Depends(get_db)]
-
-@app.get("/")
-async def read_all(db: db_dependency):
-  return db.query(Todos).all()
+app.include_router(auth.router)
+app.include_router(todos.router)
