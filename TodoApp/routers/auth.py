52fe1ed6d93d22d7a7cb68b2efc2422627b572d2
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-03 00:06:59 +0900

    モデルのプロパティ名変更による修正し、アクセストークンにユーザーのロール情報を持たせた

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -10,19 +10,21 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 from datetime import timedelta, datetime, timezone
 
-router = APIRouter()
+router = APIRouter(
+  prefix='/auth',
+  tags=['auth']
+)
 
 SECRET_KEY = 'c4644434a0567efcc411a4b377d2f9c2df6c066eca06f7791eea9e4cdbe18562'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 class CreateUserRequest(BaseModel):
   name: str
   email: str
-  createAt: None
-  photoUrl: str
+  photo_url: str
   password: str
   role: str
 
@@ -51,9 +53,9 @@ def authenticate_user(username: str, password: str, db):
   return user
 
 
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+def create_access_token(username: str, user_id: int, role: str, expires_delta: timedelta):
 
-  encode = {'sub': username, 'id': user_id}
+  encode = {'sub': username, 'id': user_id, 'role': role}
   expires = datetime.now(timezone.utc) + expires_delta
   encode.update({'exp': expires})
   return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -64,23 +66,24 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     username: str = payload.get('sub')
     user_id: str = payload.get('id')
+    user_role: str = payload.get('role')
     if username is None or user_id is None:
       raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                           detail='Could not validate user.')
-    return {'username': username, 'user_id': user_id}
+    return {'username': username, 'id': user_id, 'user_role': user_role}
   except JWTError: 
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                           detail='Could not validate user.')
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
                       create_user_request: CreateUserRequest):
   create_user_model = Users(
     name=create_user_request.name,
     email=create_user_request.email,
-    createAt=create_user_request.createAt,
-    photoUrl=create_user_request.photoUrl,
+    created_at=datetime.now(timezone.utc),
+    photo_url=create_user_request.photo_url,
     hashed_password=bcrypt_context.hash(create_user_request.password),
     role=create_user_request.role,
     is_active=True
@@ -97,6 +100,6 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
   if not user:
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                           detail='Could not validate user.')
-  token = create_access_token(user.name, user.id, timedelta(minutes=20))
+  token = create_access_token(user.name, user.id, user.role, timedelta(minutes=20))
 
   return {'access_token': token, 'token_type': 'bearer'}
