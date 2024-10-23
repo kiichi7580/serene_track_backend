19d9fea215038453cf3843d8a9134b0f6fafdb17
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-10-24 00:39:10 +0900

    feat: 環境変数でデータベースの接続先を変更する処理を追加

--- database.py
+++ database.py
@@ -1,8 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/SereneTrackDataBase'
+# 環境を取得 (デフォルトは development)
+env = os.getenv('ENV', 'development')
+
+# 環境ごとの .env ファイルを読み込む
+if env == 'development':
+    load_dotenv(dotenv_path='.env.development')
+elif env == 'production':
+    load_dotenv(dotenv_path='.env.production')
+
+# データベースURLを取得
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+
+print(f'現在の環境は {env} です')
+print(f'DATABASE_URL: {SQLALCHEMY_DATABASE_URL}')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'options': '-csearch_path=serene_track'})
 
